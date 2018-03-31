c67b8dfd81ab71f14a280f74ddcad40f367b2594
lww <2622026762@qq.com>
2018-03-31 14:37:13 +0800

    20180331

---
+++ src/test/java/fun/lww/dao/SuccessKilledDaoTest.java
@@ -0,0 +1,31 @@
+package fun.lww.dao;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import fun.lww.entity.SuccessKilled;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/spring-dao.xml")
+public class SuccessKilledDaoTest {
+
+    @Resource
+    private SuccessKilledDao successKilledDao;
+
+    @Test
+    public void insertSuccessKilled() {
+      int i = successKilledDao.insertSuccessKilled(1001L, 12312312312L);
+      System.out.println(i);
+    }
+
+    @Test
+    public void queryByIdWithSeckill() {
+        SuccessKilled successKilled = successKilledDao.queryByIdWithSeckill(1001L, 12312312312L);
+        System.out.println(successKilled);
+        System.out.println(successKilled.getSeckill());
+    }
+}
